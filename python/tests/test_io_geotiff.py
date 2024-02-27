2cf92eb458798ce1cf62195b7e58fa928c05e772
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-02-28 10:09:31 +1300

    :white_check_mark: Add unit test for reading a single-band GeoTIFF file
    Test reading an actual single-band float32 GeoTIFF file downloaded from the internet. Done in Python.

---
+++ python/tests/test_io_geotiff.py
@@ -0,0 +1,32 @@
+"""
+Test I/O on GeoTIFF files.
+"""
+import tempfile
+import urllib.request
+import os
+import pytest
+
+from cog3pio import read_geotiff
+
+
+@pytest.fixture(scope="module", name="geotiff_path")
+def fixture_geotiff_path():
+    """
+    Filepath to a sample single-band GeoTIFF file.
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+        geotiff_path = os.path.join(tmpdir, "float32.tif")
+        urllib.request.urlretrieve(
+            url="https://github.com/pka/georaster/raw/v0.1.0/data/tiff/float32.tif",
+            filename=geotiff_path,
+        )
+        yield geotiff_path
+
+
+def test_read_geotiff(geotiff_path):
+    """
+    Read a GeoTIFF file from a local file path.
+    """
+    array = read_geotiff(path=geotiff_path)
+    assert array.shape == (20, 20)
+    assert array.dtype == "float32"
