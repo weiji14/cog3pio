6cc8f4bdbab6ce00ad5df51fc8cf4a2b055f2ebd
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-03-02 07:39:41 +1300

    :test_tube: Add unit test to read GeoTIFF from remote http url
    Doing test driven development by trying to load a remote GeoTIFF file via http into a numpy array.

--- python/tests/test_io_geotiff.py
+++ python/tests/test_io_geotiff.py
@@ -8,7 +8,7 @@ import pytest
 
 from cog3pio import read_geotiff
 
-
+# %%
 @pytest.fixture(scope="module", name="geotiff_path")
 def fixture_geotiff_path():
     """
@@ -29,5 +29,17 @@ def test_read_geotiff_local(geotiff_path):
     Read a GeoTIFF file from a local file path.
     """
     array = read_geotiff(path=geotiff_path)
+    assert array.shape == (20, 20)
+    assert array.dtype == "float32"
+
+
+@pytest.mark.benchmark
+def test_read_geotiff_remote():
+    """
+    Read a GeoTIFF file from a remote URL.
+    """
+    array = read_geotiff(
+        path="https://github.com/pka/georaster/raw/v0.1.0/data/tiff/float32.tif"
+    )
     assert array.shape == (20, 20)
     assert array.dtype == "float32"
