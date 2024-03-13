1b8fac8b1b6030d48f126d130b66b2538a484fa8
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-03-13 17:42:31 +1300

    :sparkles: Get affine transformation from GeoTIFF (#8)
    * :heavy_plus_sign: Add geo
    
    Geospatial primitives and algorithms!
    
    * :sparkles: Get affine transformation from GeoTIFF
    
    Implement transform method in CogReader struct to get affine transformation matrix from GeoTIFF via ModelPixelScaleTag and ModelTiepointTag. Added a unit test to check that the x/y pixel size and top left origin pixel coordinates is correct.
    
    * :memo: Document transform method's internal properties
    
    Explicitly mentioning the internal fields of the AffineTransform struct, specifically the pixel resolution, rotation and origin, and included a reference to the GeoTIFF standard. Also renamed origin_x/origin_y to x_origin/y_origin.
    
    * :construction: Raise unimplemented when getting non-zero rotation values
    
    Supposed to be parsed from the ModelTransformationTag, but haven't found a sample GeoTIFF online with non-zero rotation to implement this properly with a unit test, so marking as unimplemented for now.

--- src/io/geotiff.rs
+++ src/io/geotiff.rs
@@ -1,8 +1,10 @@
 use std::io::{Read, Seek};
 
+use geo::AffineTransform;
 use ndarray::Array2;
 use tiff::decoder::{Decoder, DecodingResult, Limits};
-use tiff::{TiffFormatError, TiffResult};
+use tiff::tags::Tag;
+use tiff::{TiffError, TiffFormatError, TiffResult};
 
 /// Cloud-optimized GeoTIFF reader
 struct CogReader<R: Read + Seek> {
@@ -17,6 +19,55 @@ impl<R: Read + Seek> CogReader<R> {
         decoder = decoder.with_limits(Limits::unlimited());
 
         Ok(Self { decoder })
+    }
+
+    /// Affine transformation for 2D matrix extracted from TIFF tag metadata, used to transform
+    /// image pixel (row, col) coordinates to and from geographic/projected (x, y) coordinates.
+    ///
+    /// ```text
+    /// | x' |   | a b c | | x |
+    /// | y' | = | d e f | | y |
+    /// | 1  |   | 0 0 1 | | 1 |
+    /// ```
+    ///
+    /// where (`x'` and `y'`) are world coordinates, and (`x`, `y) are the pixel's image
+    /// coordinates. Letters a to f represent:
+    ///
+    /// - `a` - width of a pixel (x-resolution)
+    /// - `b` - row rotation (typically zero)
+    /// - `c` - x-coordinate of the *center* of the upper-left pixel (x-origin)
+    /// - `d` - column rotation (typically zero)
+    /// - `e` - height of a pixel (y-resolution, typically negative)
+    /// - `f` - y-coordinate of the *center* of the upper-left pixel (y-origin)
+    ///
+    /// References:
+    /// - <https://docs.ogc.org/is/19-008r4/19-008r4.html#_coordinate_transformations>
+    fn transform(&mut self) -> TiffResult<AffineTransform<f64>> {
+        // Get x and y axis rotation (not yet implemented)
+        let (x_rotation, y_rotation): (f64, f64) =
+            match self.decoder.get_tag_f64_vec(Tag::ModelTransformationTag) {
+                Ok(_model_transformation) => unimplemented!("Non-zero rotation is not handled yet"),
+                Err(_) => (0.0, 0.0),
+            };
+
+        // Get pixel size in x and y direction
+        let pixel_scale: Vec<f64> = self.decoder.get_tag_f64_vec(Tag::ModelPixelScaleTag)?;
+        let [x_scale, y_scale, _z_scale] = pixel_scale[0..3] else {
+            return Err(TiffError::FormatError(TiffFormatError::InvalidTag));
+        };
+
+        // Get x and y coordinates of upper left pixel
+        let tie_points: Vec<f64> = self.decoder.get_tag_f64_vec(Tag::ModelTiepointTag)?;
+        let [_i, _j, _k, x_origin, y_origin, _z_origin] = tie_points[0..6] else {
+            return Err(TiffError::FormatError(TiffFormatError::InvalidTag));
+        };
+
+        // Create affine transformation matrix
+        let transform = AffineTransform::new(
+            x_scale, x_rotation, x_origin, y_rotation, -y_scale, y_origin,
+        );
+
+        Ok(transform)
     }
 }
 
@@ -42,12 +93,15 @@ pub fn read_geotiff<R: Read + Seek>(stream: R) -> TiffResult<Array2<f32>> {
 
 #[cfg(test)]
 mod tests {
-    use std::io::{Seek, SeekFrom};
+    use std::io::{Cursor, Seek, SeekFrom};
 
+    use geo::AffineTransform;
+    use object_store::parse_url;
     use tempfile::tempfile;
     use tiff::encoder::{colortype, TiffEncoder};
+    use url::Url;
 
-    use crate::io::geotiff::read_geotiff;
+    use crate::io::geotiff::{read_geotiff, CogReader};
 
     #[test]
     fn test_read_geotiff() {
@@ -75,5 +129,25 @@ mod tests {
         assert_eq!(arr.nrows(), 10); // y-axis
         assert_eq!(arr.ncols(), 20); // x-axis
         assert_eq!(arr.mean(), Some(14.0));
+    }
+
+    #[tokio::test]
+    async fn test_cogreader_transform() {
+        let cog_url: &str =
+            "https://github.com/cogeotiff/rio-tiler/raw/6.4.0/tests/fixtures/cog_nodata_nan.tif";
+        let tif_url = Url::parse(cog_url).unwrap();
+        let (store, location) = parse_url(&tif_url).unwrap();
+
+        let result = store.get(&location).await.unwrap();
+        let bytes = result.bytes().await.unwrap();
+        let stream = Cursor::new(bytes);
+
+        let mut reader = CogReader::new(stream).unwrap();
+        let transform = reader.transform().unwrap();
+
+        assert_eq!(
+            transform,
+            AffineTransform::new(200.0, 0.0, 499980.0, 0.0, -200.0, 5300040.0)
+        );
     }
 }
