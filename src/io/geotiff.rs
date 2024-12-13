b1785190fe311b26c00c422df186af61ffdbc710
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-12-13 15:33:05 -0500

    :rotating_light: Fix error: useless conversion to the same type

--- src/io/geotiff.rs
+++ src/io/geotiff.rs
@@ -79,11 +79,9 @@ impl<R: Read + Seek> CogReader<R> {
         };
 
         // Put image pixel data into an ndarray
-        let array_data: Array3<T> = Array3::from_shape_vec(
-            (num_bands, height as usize, width as usize),
-            image_data.into(),
-        )
-        .map_err(|_| TiffFormatError::InconsistentSizesEncountered)?;
+        let array_data: Array3<T> =
+            Array3::from_shape_vec((num_bands, height as usize, width as usize), image_data)
+                .map_err(|_| TiffFormatError::InconsistentSizesEncountered)?;
 
         Ok(array_data)
     }
