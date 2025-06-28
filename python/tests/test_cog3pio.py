5078594aac2b6a0512d24cbdd1ff71770ece0190
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-06-28 18:25:33 +1200

    :arrow_up: Bump cog3pio from 0.0.1b5 to 0.0.1
    Bumps [cog3pio](https://github.com/weiji14/cog3pio) from 0.0.1b5 to 0.0.1.
    - [Release notes](https://github.com/weiji14/cog3pio/releases)
    - [Changelog](https://github.com/weiji14/cog3pio/blob/main/docs/changelog.md)
    - [Commits](https://github.com/weiji14/cog3pio/compare/0.0.1b5...0.0.1)

--- python/tests/test_cog3pio.py
+++ python/tests/test_cog3pio.py
@@ -4,4 +4,4 @@ from cog3pio import __version__
 
 
 def test_version():
-    assert Version(version=__version__) >= Version(version="0.0.0")
+    assert Version(version=__version__) >= Version(version="0.0.1")
