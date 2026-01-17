536f89444010d136d8b3b9bd04551107018c7cfd
Wei Ji <23487320+weiji14@users.noreply.github.com>
2026-01-17 17:37:59 +1300

    :white_check_mark: Benchmark TIFF decode to cupy for cog3pio vs rasterio
    Compare reading into a cupy.ndarray between cog3pio and rasterio. First one is using cog3pio.CudaCogReader followed by a cupy.from_dlpack; second one is using rasterio.open+read followed by cupy.asarray. Using cupyx.profiler.benchmark to calculate both GPU and CPU times, reported to stdout.

--- python/tests/test_io_nvtiff.py
+++ python/tests/test_io_nvtiff.py
@@ -2,13 +2,25 @@
 Test GeoTIFF I/O via nvTIFF backend.
 """
 
+import sys
+
 import pytest
 
 cp = pytest.importorskip("cupy")
 
+import cupyx.profiler
+import numpy as np
 from cog3pio import CudaCogReader
 
+try:
+    import rasterio
 
+    HAS_RASTERIO = True
+except ImportError:
+    HAS_RASTERIO = False
+
+
+# %%
 def test_cudacogreader_to_dlpack():
     """
     Ensure that the CudaCogReader class's `__dlpack__` method produces a dl_tensor that
@@ -29,3 +41,47 @@ def test_cudacogreader_to_dlpack():
         actual=array,
         desired=cp.array([1.41, 1.23, 0.78, 0.32, -0.23, -1.88], dtype=cp.float32),
     )
+
+
+@pytest.mark.benchmark
+@pytest.mark.parametrize(
+    "engine",
+    [
+        "cog3pio",
+        pytest.param(
+            "rasterio",
+            marks=pytest.mark.skipif(
+                condition=not HAS_RASTERIO, reason="Could not import 'rasterio'"
+            ),
+        ),
+    ],
+)
+def test_benchmark_tocupy(engine):
+    """
+    Benchmark cog3pio.CudaCogReader decoding semi-directly to cupy.ndarray, against
+    equivalent rasterio.open to numpy.ndarray, followed by host to device copy to
+    cupy.ndarray.
+    """
+    tiff: str = "https://github.com/developmentseed/titiler/raw/1.1.0/src/titiler/core/tests/fixtures/TCI.tif"
+
+    match engine:
+        case "cog3pio":
+
+            def cog_to_cupy():
+                with cp.cuda.Stream(ptds=True):  # use per-thread default stream
+                    cog = CudaCogReader(path=tiff)
+                    array: cp.ndarray = cp.from_dlpack(cog)
+                    assert array.shape == (3616812,)
+
+        case "rasterio":
+
+            def cog_to_cupy():
+                with rasterio.open(fp=tiff) as dataset:
+                    bands: np.ndarray = dataset.read(indexes=[1, 2, 3])
+                    array: cp.ndarray = cp.asarray(a=bands)
+                    assert array.shape == (3, 1098, 1098)
+
+    # Run benchmark with GPU and CPU timings, report to stdout
+    perf = cupyx.profiler.benchmark(func=cog_to_cupy, n_repeat=1)
+    perf.name += f"[{engine}]\t"
+    print(perf, file=sys.stdout)
